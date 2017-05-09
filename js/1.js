5f0a8662cc52359743e6d18a1cb44188bef7bbbe
fengsai <356263964@qq.com>
2017-05-09 14:08:07 +0800

    js做了处理

--- js/1.js
+++ js/1.js
@@ -2,8 +2,33 @@
  * Created by Administrator on 2017/5/8.
  */
 
-(function () {
+(function (w) {
 
-    alert(1)
+    function F(id,content) {
+        return this.init(id,content)
+    }
 
-})();
+    F.prototype={
+
+        init:function (id,content) {
+            var nodelist=(content||document).querySelectorAll(id);
+             this.lenght=nodelist.length;
+
+            for(var i=0;i<nodelist.length;i++)
+            {
+                this[i]=nodelist[i];
+            }
+
+            return this;
+
+        }
+
+    }
+
+    var $=function (id,content) {
+        return F(id,content);
+    }
+
+
+
+})(window);
